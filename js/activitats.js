db19fcd4f8fa919e99f3dbacf40ed59c1756d71f
thePLO <pdemiguel@enti.cat>
2021-12-20 13:29:46 +0100

    ferrates

--- js/activitats.js
+++ js/activitats.js
@@ -127,9 +127,24 @@ let nivells = [
 	{nom: "Molt difícil +", color:"#000000"}		//6
 ]
 let nivellClassificació = [
-	{nom: "Iniciació", color:"#00ff00", img:"source/img/slideInici/foto (4).webp", color:"#7FFF00"},		//0
-	{nom: "Mig", color:"#ffff00", img:"source/img/slideInici/foto (4).webp", color:"#ffff00"},				//1
-	{nom: "Avançat", color:"#ff0000", img:"source/img/slideInici/foto (4).webp", color:"#ff0000"}			//2
+	{
+		nom: "Iniciació",
+		descripcio: "Ideal per un primer contacte, faamílies i grups d'amics sense exxperiència",
+		color:"#00ff00",
+		img:"source/img/slideInici/foto (4).webp"
+	},//0
+	{
+		nom: "Mig",
+		descripcio: "Per a persones i grups que ja hagin tingut un primer contacte amb les vies ferrades",
+		color:"#ffff00",
+		img:"source/img/slideInici/foto (4).webp"
+	},				//1
+	{
+		nom: "Avançat",
+		descripcio: "Per a persones atrevides i valentes que tinguin experiència i bon nivell de condició física",
+		color:"#ff0000",
+		img:"source/img/slideInici/foto (4).webp"
+	}			//2
 ]
 let activitats = [
 	{
@@ -168,6 +183,216 @@ let activitats = [
 			},
 			{
 				"ca": "Us animeu?",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "ROCA NARIEDA (TRAM FAMILIAR)",
+		lloc: "FÍGOLS I ALINYÀ, ALT URGELL, LLEIDA",
+		destacada: false,
+		nivell: 1,
+		nivellClassificacio:0,
+		preu: "40€",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "TRAM FAMILIAR.",
+				"es": ""
+			},
+			{
+				"ca": "Primer tram de la via ferrada ideal per iniciar-se i venir amb els mes petits de la casa. Una horeta de recorregut equipat i un camí que ens tornarà al punt d'inici.",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "TELETUBBIES",
+		lloc: "CORÇA, LA NOGUERA, LLEIDA",
+		destacada: false,
+		nivell: 1,
+		nivellClassificacio:0,
+		preu: "45€",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Magnífic recorregut per iniciar-se, equipat amb gran varietat d’elements ens enfilarem per una cresta on gaudirem d’unes magnífiques vistes del pantà de Canelles. Molt recomanada per a famílies i petits!",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "OLMO - SOLER",
+		lloc: "CORÇA, LA NOGUERA, LLEIDA",
+		destacada: false,
+		nivell: 1,
+		nivellClassificacio:0,
+		preu: "45€",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Vols visitar l’ermita de la Mare de Déu de la Pertusa? Doncs ja no tens excusa! Una via senzilla i entretinguda que transcorre per curts ressalts molt ben equipats amb el pantà de Canelles als nostres peus!",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "PATACONS",
+		lloc: "MUSARA, BAIX CAMP, TARRAGONA",
+		destacada: false,
+		nivell: 2,
+		nivellClassificacio:0,
+		preu: "45€",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Gairebé 500 metres de longitud, una passarel·la de fusta i 4 ponts nepalís és el recorregut d'aquesta via. Si no et fan por les alçades i vols gaudir d’una de les grans ferrades catalanes, no dubtis a venir.",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "BAUMES CORCADES",
+		lloc: "CENTELLES, OSONA, BARCELONA",
+		destacada: true,
+		nivell: 2,
+		nivellClassificacio:0,
+		preu: "35€",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Coneguda i famosa pel seu impressionant pont nepalí de 68 metres. També trobarem trams verticals, desplomats atlètics i una escala penjant!",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "ROCA NARIEDA",
+		lloc: "FÍGOLS I ALINYÀ, ALT URGELL, LLEIDA",
+		destacada: false,
+		nivell: 2,
+		nivellClassificacio:0,
+		preu: "55€",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "La via ferrada més llarga de la península, amb una escala en forma d’espiral i una passarel·la exposada de 38 metres! No tens ganes de descobrir aquesta via? Ho té tot: és llarga, exigent i amb passos originals.",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "SERRA DE LES CANALS",
+		lloc: "OLIANA, ALT URGELL, LLEIDA",
+		destacada: false,
+		nivell: 2,
+		nivellClassificacio:0,
+		preu: "Consultar",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Disposa d'un primer tram més familiar i d'un segon mes vertical i atlètic per si ens quedem amb ganes. Un retorn equipat per la cresta que ens farà acabar de gaudir la jornada.",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "VENES DE ROJALONS",
+		lloc: "ROJALONS, CONCA DE BARBERÀ, TARRAGONA",
+		destacada: false,
+		nivell: 2,
+		nivellClassificacio:0,
+		preu: "Consultar",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Has fet alguna ferrada a les fosques? Aquí trobarem una mica de tot! Recorregut molt original, amb algun tram on necessitarem portar frontal, ja que va per dins d'unes esquerdes! T’hi animes?",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "CANAL DE LES DAMES",
+		lloc: "COLLBATÓ, BAIX LLOBREGAT, BARCELONA",
+		destacada: false,
+		nivell: 4,
+		nivellClassificacio:0,
+		preu: "45€",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Bones vistes i seguretat són dos al·licients que ofereix la via ferrada Canal de les Dames, una via divertida que compleix les expectatives de qualsevol. Vine a gaudir del conglomerat de Montserrat amb nosaltres!",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "CANAL DEL MEJILLÓN",
+		lloc: "MONTSERRAT, BAGES, BARCELONA",
+		destacada: false,
+		nivell: 4,
+		nivellClassificacio:0,
+		preu: "Consultar",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Equipada amb cadenes i un parell de desploms, aquesta canal equipada farà tirar de força de braços i posar a prova els més valents.",
+				"es": ""
+			},
+			{
+				"ca": "Vols saber perquè té aquest nom?",
+				"es": ""
+			}
+		]
+	},
+	{
+		tipus: 0,
+		nom: "CAST - URQUIZA - OLMO",
+		lloc: "CORÇA, NOGUERA, LLEIDA",
+		destacada: false,
+		nivell: 4,
+		nivellClassificacio:0,
+		preu: "Consultar",
+		foto:"",
+		video:"",
+		link:"",
+		descripcio: [
+			{
+				"ca": "Esportiva, aèria i vertical, caracteritzada com una de les vies més difícils del país, equipada d’una manera molt particular que la fa encara més complicada.",
+				"es": ""
+			},
+			{
+				"ca": "T’hi atreveixes?",
 				"es": ""
 			}
 		]
